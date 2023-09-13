7c2982a46de3946453ce8475bc7d599137f7a2ba
StashaSesevic <100646235+StashaSesevic@users.noreply.github.com>
2023-09-13 20:37:07 +0200

    Update script04.js

--- script/script04.js
+++ script/script04.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
     $('#forma01').submit(function(event) {
         event.preventDefault();
-        // Vaša postojeća validacija
-
+    
         $.ajax({
             type: 'POST',
-            url: 'forma.php', // Putanja do vaše PHP skripte
+            url: 'forma.php', 
             data: $('#forma01').serialize(),
             success: function(response) {
-                alert(response); // Ovde možete prikazati odgovor sa servera
+                alert(response); 
             }
         });
     });
